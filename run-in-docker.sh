73863251ea705c5167ef0ac80610ccb0b6eec6e5
rodriguezst <2828844+rodriguezst@users.noreply.github.com>
2024-06-26 00:50:52 +0200

    Remove defconfig

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -3,7 +3,6 @@ export DEBIAN_FRONTEND=noninteractive
 apt update
 apt -y install build-essential bc flex bison bash make
 cd kernel 
-make xiaomi_nabu_maverick_defconfig
 make headers_install 
 cd ../qbootctl 
 make clean all
