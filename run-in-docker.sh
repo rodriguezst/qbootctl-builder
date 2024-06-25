45b0b27edc2cd2d35c4a2d8a7f2198281d7dc2b7
rodriguezst <2828844+rodriguezst@users.noreply.github.com>
2024-06-26 00:21:14 +0200

    Update run-in-docker.sh

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-apt update && sudo apt install build-essential bc flex bison 7zip bash cpio binutils tar git wget libssl-dev
+apt update
+apt install build-essential bc flex bison 7zip bash cpio binutils tar git wget libssl-dev make
 cd kernel 
 make xiaomi_nabu_maverick_defconfig
 make headers_install 
