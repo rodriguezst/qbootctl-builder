bda3e60c55c2b7d5beccb7d15d7286fe7500db75
rodriguezst <2828844+rodriguezst@users.noreply.github.com>
2024-06-26 10:24:55 +0200

    Remove no longer needed run-in-docker.sh

--- run-in-docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export DEBIAN_FRONTEND=noninteractive
-apt update
-apt -y install make rsync zlib1g-dev meson cmake
-cd kernel 
-make headers_install 
-cd ../qbootctl 
-meson build
-meson compile -C ../build
